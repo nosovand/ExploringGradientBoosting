7bd83041fe9d193d3792f92bacb74ee60486e717
Andrey Nosov <a.nosoff00@gmail.com>
2024-06-13 20:16:38 +0200

    Fixed regression tree splitter
    Fixed problem with data type, which incorrectly sorted feature values

--- my_tree.py
+++ my_tree.py
@@ -345,6 +345,20 @@ class CustomDecisionTreeClassifier(BaseEstimator, ClassifierMixin):
 
 class CustomDecisionTreeRegressor(BaseEstimator, RegressorMixin):
     def __init__(self, max_depth=1, min_samples_split=2, min_samples_leaf=1, max_features=None):
+        '''
+        Constructor for the DecisionTreeRegressor class.
+
+        Parameters:
+        max_depth: int, default=None
+            The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.
+        min_samples_split: int, default=2
+            The minimum number of samples required to split an internal node.
+        min_samples_leaf: int, default=1
+            The minimum number of samples required to be at a leaf node.
+        max_features: int or string, default=None
+            The number of features to consider when looking for the best split. If None, then all features will be considered. If 'sqrt', then max_features=sqrt(n_features). If 'log2', then max_features=log2(n_features).
+        '''
+    
         if not isinstance(max_depth, int):
             raise ValueError(f"max_depth must be an integer. {max_depth} is not an integer.")
         else:
@@ -363,9 +377,21 @@ class CustomDecisionTreeRegressor(BaseEstimator, RegressorMixin):
             self.max_features = max_features
     
     def __call__(self, *args, **kwargs):
+        '''
+        Initializes the tree with the new parameters
+        '''
         return CustomDecisionTreeRegressor(*args, **kwargs)
 
     def fit(self, X, y):
+        '''
+        Build a decision tree regressor from the training set (X, y).
+
+        Parameters:
+        X: matrix of shape (n_samples + feature_name, n_features)
+            The training input samples.
+        y: array-like of shape (n_samples)
+            The target values.
+        '''
         X = self._check_feature_format(X)
         y = np.array(y)
         if self.max_features is None:
@@ -380,145 +406,283 @@ class CustomDecisionTreeRegressor(BaseEstimator, RegressorMixin):
         self.tree = self._build_tree(X, y, depth=0)
 
     def _build_tree(self, X, y, depth):
+        '''
+        Recursively build the tree.
+
+        Parameters:
+        X: matrix of shape (n_samples, n_features)
+            The training input samples.
+        y: array-like of shape (n_samples)
+            The target values.
+        depth: int
+            The current depth of the tree.
+        
+        Returns:
+        Node: The root node of the tree.
+        '''
+
+        # If the tree is empty, return None
+        # Used only for debugging purposes
         if self._empty_tree(y):
             return None
-        if self._stop_condition(X, y, depth):
-            return np.mean(y)
+        
+        # If the stopping condition is met, return the mean of the target values
+        if self._stop_condition(y, depth):
+            return np.mean(y.astype(float)).astype(float)
 
+        # Find the best split
         best_feature, best_value, best_mse = self._find_best_split(X, y)
-        if best_feature is None or best_value is None:
-            return np.mean(y)
 
-        best_feature_name = X[0, best_feature]
-        # print(f"Best feature: {best_feature_name}, Best value: {best_value}, Best MSE: {best_mse}")
-        # print(f"Best feature sample: {X[1:, best_feature]}")
-        # print(f"Best feature sample as float: {X[1:, best_feature].astype(float)}")
+        # Check if the best split is None
+        if best_feature is None or best_value is None:
+            # Best split was not found, return the mean of the target values
+            return np.mean(y.astype(float)).astype(float)
+
         left_indices = X[1:, best_feature].astype(float) <= best_value
         right_indices = X[1:, best_feature].astype(float) > best_value
         left_indices = np.insert(left_indices, 0, True)
         right_indices = np.insert(right_indices, 0, True)
 
+        # Check for the min_samples_leaf condition
         if len(y[left_indices[1:]]) < self.min_samples_leaf or len(y[right_indices[1:]]) < self.min_samples_leaf:
-            return np.mean(y)
+            return np.mean(y.astype(float)).astype(float)
 
+        # Recursively build the tree
         left_node = self._build_tree(X[left_indices], y[left_indices[1:]], depth + 1)
         right_node = self._build_tree(X[right_indices], y[right_indices[1:]], depth + 1)
         
-        tree = Node(feature=best_feature, value=best_value, left=left_node, right=right_node)
+        # Create a node
+        best_feature_name = X[0, best_feature]
+        tree = Node(feature=best_feature, feature_name=best_feature_name, value=best_value, left=left_node, right=right_node)
         return tree
     
-    def _stop_condition(self, X, y, depth) -> bool:
+    def _stop_condition(self, y, depth) -> bool:
+        '''
+        Check if the stopping condition is met.
+        Stopping conditions:
+        - The current depth of the tree is equal to the maximum depth.
+        - The number of samples is less than the minimum number of samples required to split an internal node.
+
+        Parameters:
+        y: array-like of shape (n_samples)
+            The target values.
+        depth: int
+            The current depth of the tree.
+        
+        Returns:
+        bool: True if the stopping condition is met, False otherwise.
+        '''
         return depth == self.max_depth or len(y) < self.min_samples_split
     
     def _empty_tree(self, y) -> bool:
+        '''
+        Check if the tree is empty.
+
+        Parameters:
+        y: array-like of shape (n_samples,)
+            The target values.
+
+        Returns:
+        bool: True if the tree is empty, False otherwise.
+        '''
         return len(y) == 0
     
     def _find_best_split(self, X, y):
+        '''
+        Find the best split for the data by iterating over all features and values
+        to find the one that minimizes the mean squared error.
+
+        Parameters:
+        X: matrix of shape (n_samples + feature_name, n_features)
+            The training input samples.
+        y: array-like of shape (n_samples,)
+            The target values.
+        
+        Returns:
+        int: The index of the best feature.
+        float: The best value to split the data.
+        float: The best mean squared error.
+        '''
+
         number_of_features = X.shape[1]
         best_mse = float("inf")
         best_feature = None
         best_value = None
         self.current_node_num_samples = len(y)
 
+        # Keep only max_features number of features
         if self.max_features < number_of_features:
             random_indices = np.random.choice(X.shape[1], self.max_features, replace=False)
             X = X[:, random_indices]
 
-        sorted_indices = np.argsort(X[1:, :], axis=0)
+        # Sort the data along each feature
+        sorted_indices = np.argsort(X[1:, :].astype(float), axis=0)        
         sorted_X = np.take_along_axis(X[1:, :], sorted_indices, axis=0)
 
         for feature in range(X.shape[1]):
-            # print("Feature", feature)
             sorted_y = np.take_along_axis(y, sorted_indices[:, feature], axis=0)
-            sorted_x_uniq, counts_x = np.unique(sorted_X[:, feature], return_counts=True)
+            # We want to keep only the unique values of the feature for splitting
+            sorted_x_uniq, counts_x = np.unique(sorted_X[:, feature].astype(float), return_counts=True)
             if len(sorted_x_uniq) == 1:
                 continue
             
-            sorted_y_sum_uniq_x = np.zeros_like(counts_x, dtype=float)
-            start_index = 0
-            for idx, count in enumerate(counts_x):
-                end_index = start_index + count
-                sorted_y_sum_uniq_x[idx] = np.sum(sorted_y[start_index:end_index])
-                if idx > 0:
-                    sorted_y_sum_uniq_x[idx] += sorted_y_sum_uniq_x[idx - 1]
-                start_index = end_index
-            
+            # Calculate the sizes of the left and right splits
+            left_sizes = np.append([0], np.cumsum(counts_x))
+            right_sizes = self.current_node_num_samples - left_sizes
+
+            # Compute the cumulative sum of sorted_y for unique x values
+            cumsum_sorted_y = np.cumsum(sorted_y)
+            end_indices = np.cumsum(counts_x)
+            start_indices = np.roll(end_indices, shift=1)
+            start_indices[0] = 0
+            sorted_y_sum_uniq_x = cumsum_sorted_y[end_indices - 1]
             rev_sorted_y_sum_uniq_x = sorted_y_sum_uniq_x[-1] - sorted_y_sum_uniq_x
             
-            left_sizes = np.cumsum(counts_x)
-            right_sizes = self.current_node_num_samples - left_sizes
-            
-            left_means = sorted_y_sum_uniq_x[:-1] / left_sizes[:-1]
-            right_means = rev_sorted_y_sum_uniq_x[:-1] / right_sizes[:-1]
-            
+            # Calculate the means of the left and right splits
+            left_means = sorted_y_sum_uniq_x[:-1] / left_sizes[1:-1]
+            right_means = rev_sorted_y_sum_uniq_x[:-1] / right_sizes[1:-1]
+
+            # Calculate the MSE for all possible splits using only the unique values of the feature
             left_mse_array = np.zeros(len(left_means))
             right_mse_array = np.zeros(len(right_means))
             
             for i in range(len(left_means)):
-                left_mse_array[i] = np.mean((sorted_y[:left_sizes[i]] - left_means[i]) ** 2)
-                right_mse_array[i] = np.mean((sorted_y[left_sizes[i]:] - right_means[i]) ** 2)
-
-            # left_mse_array = np.array([np.mean((sorted_y[:size] - mean) ** 2) for size, mean in zip(left_sizes[:-1], left_means)])
-            # right_mse_array = np.array([np.mean((sorted_y[size:] - mean) ** 2) for size, mean in zip(left_sizes[:-1], right_means)])
-
+                left_mse_array[i] = np.mean((sorted_y[:left_sizes[i+1]] - left_means[i]) ** 2)
+                right_mse_array[i] = np.mean((sorted_y[left_sizes[i+1]:] - right_means[i]) ** 2)
             
-            mse_array = (left_sizes[:-1] / self.current_node_num_samples) * left_mse_array + \
-                        (right_sizes[:-1] / self.current_node_num_samples) * right_mse_array
+            mse_array = (left_sizes[1:-1] / self.current_node_num_samples) * left_mse_array + \
+                        (right_sizes[1:-1] / self.current_node_num_samples) * right_mse_array
             
-            min_mse_idx = np.argmin(mse_array)
+            
+            # Choose the best split while checking for the min_samples_leaf condition using the mse_array
+            splits_considered = 0
+            while splits_considered < len(mse_array):
+                min_mse_idx = np.argmin(mse_array)
+                if left_sizes[min_mse_idx+1] < self.min_samples_leaf or right_sizes[min_mse_idx+1] < self.min_samples_leaf:
+                    mse_array[min_mse_idx] = float("inf")
+                    splits_considered += 1
+                else:
+                    break
+            
             if mse_array[min_mse_idx] < best_mse:
                 best_mse = mse_array[min_mse_idx]
                 best_feature = feature
+                # Since the mse_array is calculated for the unique values of the feature,\
+                #  we need to find the index of the best value in the original sorted_X
                 best_value_idx = np.cumsum(counts_x)[min_mse_idx] - 1
-                
-                if best_value_idx == len(sorted_X) - 1:
-                    best_value = sorted_X[best_value_idx, feature]
-                else:
-                    best_value = (sorted_X[best_value_idx + 1, feature].astype(float))
+                best_value = sorted_X[best_value_idx, feature].astype(float)
 
+        # Translate the index of the feature to the original index if max_features < number_of_features
         if self.max_features < number_of_features:
             best_feature = random_indices[best_feature]
 
         return best_feature, best_value, best_mse
 
     def predict(self, X):
+        '''
+        Predict target values for X.
+
+        Parameters:
+        X: matrix of shape (n_samples + feature_name, n_features)
+            The input samples.
+        
+        Returns:
+        array-like of shape (n_samples)
+            The predicted target values.
+        '''
         X = self._check_feature_format(X)[1:]
         return np.array([self._predict_tree(x, self.tree) for x in X])
     
     def _predict_tree(self, x, node):
+        '''
+        Recursively predict the target value for a given input sample x.
+
+        Parameters:
+        x: array-like of shape (n_features,)
+            The input sample.
+        node: Node
+            The current node in the tree.
+
+        Returns:
+        Any: The predicted target value.
+        '''
+
+        # If the node is a leaf, return the predicted target value
         if not isinstance(node, Node):
             return node
+        
+        # Recursively traverse the tree until a leaf is reached
         if x[node.feature].astype(float) <= node.value:
             return self._predict_tree(x, node.left)
         else:
             return self._predict_tree(x, node.right)
         
     def _check_feature_format(self, data):
+        '''
+        Converts a DataFrame to a NumPy matrix.
+
+        Parameters:
+        data (DataFrame): The DataFrame to be converted.
+
+        Returns:
+        numpy.ndarray: NumPy matrix containing the values of the DataFrame.
+        '''
+
         if not isinstance(data, pd.DataFrame):
             raise ValueError("Input data must be a pandas DataFrame.")
         column_names = data.columns.tolist()
         return np.vstack([column_names, data.values])
     
     def draw_tree(self):
+        '''
+        Draw the tree.
+        '''
+
         self._draw_tree(self.tree)
 
     def _draw_tree(self, node, depth=0):
+        '''
+        Recursively draw the tree using visual indentation.
+
+        Parameters:
+        node: Node
+            The current node in the tree.
+        depth: int
+            The current depth of the tree.
+        '''
+
+        # If the node is a leaf, print the predicted target value
         if not isinstance(node, Node):
             print("  " * depth, node)
             return
-        print("  " * depth, f"Feature {node.feature} <= {node.value}")
+        
+        # Recursively draw the tree
+        print("  " * depth, f"Feature {node.feature} < {node.value}")
         self._draw_tree(node.left, depth + 1)
-        print("  " * depth, f"Feature {node.feature} > {node.value}")
+        print("  " * depth, f"Feature {node.feature} >= {node.value}")
         self._draw_tree(node.right, depth + 1)
 
     def score(self, X, y):
+        '''
+        Return the coefficient of determination R^2 of the prediction.
+
+        Parameters:
+        X: matrix of shape (n_samples + feature_name, n_features)
+            The input samples.
+        y: array-like of shape (n_samples)
+            The target values.
+
+        Returns:
+        float: The coefficient of determination R^2 of the prediction.
+        '''
+
         predictions = self.predict(X)
         u = np.sum((predictions - y) ** 2)
         v = np.sum((y - np.mean(y)) ** 2)
         return 1 - u/v
 
     def get_params(self, deep=True):
-        return {"max_depth": self.max_depth, "min_samples_split": self.min_samples_split}
+        return {"max_depth": self.max_depth, "min_samples_split": self.min_samples_split, "min_samples_leaf": self.min_samples_leaf, "max_features": self.max_features}
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
